d87609542f010803fae99904d4649ae0c9525021
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-21 15:26:38 +0400

    Task 3 Solution: Integer countIs = 0;

--- src/equalIsNot.java
+++ src/equalIsNot.java
@@ -1,16 +1,20 @@
 public class equalIsNot {
     public static void main(String[] args) {
-        System.out.println(equalIsNot("This is not"));
+        System.out.println(equalIsNot("noisxxnotyynotxisi"));
     }
-    public static Integer equalIsNot(String str){
+    public static Boolean equalIsNot(String str){
         Integer countIs = 0;
         Integer countNot = 0;
         for(int i=0; i<str.length()-1; i++){
             if(str.substring(i,i+2).equals("is")){
                 countIs++;
             }
+        }for(int j=0; j<str.length()-2; j++){
+            if(str.substring(j,j+3).equals("not")){
+                countNot++;
+            }
         }
-        return countIs;
+        return countNot==countIs;
     }
 
 }
