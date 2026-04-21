2a2073a73bbee3714d5819392b275ec451e74608
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-21 14:36:27 +0400

    Task1 solution : countYZ

--- src/countYZ.java
+++ src/countYZ.java
@@ -1,5 +1,21 @@
 public class countYZ {
     public static void main(String[] args) {
+        System.out.println(countYZ("fez day"));
+
+    }
+    public static Integer countYZ(String str){
+        Integer count =0;
+        String [] strings = str.split("[^a-zA-Z]");
+        for(String s: strings){
+            if(s.length() > 0){
+                String last = s.substring(s.length()-1);
+
+                if(last.equalsIgnoreCase("y") || last.equalsIgnoreCase("z") ){
+                    count++;
+                }}
+
+        }
+        return count;
 
     }
 }
