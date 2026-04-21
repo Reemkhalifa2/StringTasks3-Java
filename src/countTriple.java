82fe9ccccb800859a9a10d29fff7fde1856350ec
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-21 15:59:51 +0400

    Task 5 solution: Integer count = 0;

--- src/countTriple.java
+++ src/countTriple.java
@@ -1,8 +1,15 @@
 public class countTriple {
     public static void main(String[] args) {
+        System.out.println(countTriple("a") );
 
     }
     public static Integer countTriple(String str){
-
+        Integer count = 0;
+        for(int i=0; i<str.length()-2;i++){
+            if(str.charAt(i)==str.charAt(i+1) && str.charAt(i+1)==str.charAt(i+2)){
+                count++;
+            }
+        }
+        return count;
     }
 }
