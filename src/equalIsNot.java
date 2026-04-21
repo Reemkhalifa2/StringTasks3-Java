dd012a8740747dbfe364bb543782cbaedbff1dbd
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-21 15:24:36 +0400

    version 1 task 3 solution

--- src/equalIsNot.java
+++ src/equalIsNot.java
@@ -1,2 +1,16 @@
 public class equalIsNot {
+    public static void main(String[] args) {
+        System.out.println(equalIsNot("This is not"));
+    }
+    public static Integer equalIsNot(String str){
+        Integer countIs = 0;
+        Integer countNot = 0;
+        for(int i=0; i<str.length()-1; i++){
+            if(str.substring(i,i+2).equals("is")){
+                countIs++;
+            }
+        }
+        return countIs;
+    }
+
 }
