4998aafb165eb19271b23bdf40e90a3ac4b2a910
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-21 15:55:06 +0400

    Task4 solution: gHappy

--- src/gHappy.java
+++ src/gHappy.java
@@ -1,2 +1,19 @@
 public class gHappy {
+    public static void main(String[] args) {
+        System.out.println(gHappy("xxgxx"));
+    }
+    public static Boolean gHappy(String str){
+        Boolean found = true;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == 'g'){
+                if( (i > 0 && str.charAt(i-1) == 'g') ||
+                        (i < str.length()-1 && str.charAt(i+1) == 'g') ){
+                    found = true;
+                } else {
+                    found = false;
+                }
+            }
+        }
+        return found;
+    }
 }
