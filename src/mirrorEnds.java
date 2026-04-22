72e7cdbf3f193514baebf7ded6ff0343a49cf97e
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-22 08:53:05 +0400

    Task 8 solution: mirrorEnds

---
+++ src/mirrorEnds.java
@@ -0,0 +1,16 @@
+public class mirrorEnds {
+    public static void main(String[] args) {
+        System.out.println(mirrorEnds("aba"));
+    }
+    public static String mirrorEnds(String str){
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(str.length() - 1 - i)) {
+                result += str.charAt(i);
+            } else {
+                break;
+            }
+        }
+        return result;
+    }
+}
