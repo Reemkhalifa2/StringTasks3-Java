d292006ab20938b7bfdb42c340b57fc46bcab1e7
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-22 08:31:29 +0400

    Task 7 solution: sameEnds

--- src/sameEnds.java
+++ src/sameEnds.java
@@ -1,2 +1,17 @@
 public class sameEnds {
+    public static void main(String[] args) {
+        System.out.println(sameEnds("xx"));
+
+    }
+    public static String sameEnds(String str) {
+        String result = "";
+        for(int i=0; i<str.length()/2;i++){
+            if(str.substring(0,i).equals( str.substring(str.length()-i))){
+                result += str.substring(0,i);
+            }
+        }
+        return result;
+
+
+    }
 }
