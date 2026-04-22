51b3f5d1f92fc26dda004f8c95086e6fafa5254d
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-22 09:34:47 +0400

    Task 11 solution: notReplace

--- src/notReplace.java
+++ src/notReplace.java
@@ -1,18 +1,23 @@
-public String notReplace(String str) {
-  String result = "";
-
-  for (int i = 0; i < str.length(); i++) {
-    if (i <= str.length() - 2 &&
-        str.substring(i, i + 2).equals("is") &&
-        (i == 0 || !Character.isLetter(str.charAt(i - 1))) &&
-        (i + 2 >= str.length() || !Character.isLetter(str.charAt(i + 2)))) {
-      
-      result += "is not";
-      i++;
-    } else {
-      result += str.charAt(i);
+public class notReplace {
+    public static void main(String[] args) {
+        System.out.println(notReplace("is test"));
     }
-  }
+    public static String notReplace(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (i <= str.length() - 2 &&
+                    str.substring(i, i + 2).equals("is") &&
+                    (i == 0 || !Character.isLetter(str.charAt(i - 1))) &&
+                    (i + 2 >= str.length() || !Character.isLetter(str.charAt(i + 2)))) {
 
-  return result;
+                result += "is not";
+                i++;
+            } else {
+                result += str.charAt(i);
+            }
+        }
+
+        return result;
+    }
+
 }
